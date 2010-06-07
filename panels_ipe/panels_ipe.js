8a6efca925cdbe5c0ea0d1cbc26b078f166d49cc
Sam Boyer <drupal@samboyer.org>
2010-06-07 20:05:05 +0000

    Basic functionality is working.
    While it makes sense in retrospect, jQuery UI's sortable takes care of
    generating its own draggable & droppable logic - a fact that's doesn't
    seem to be presented quite as up-front as it ought to be!

--- panels_ipe/panels_ipe.js
+++ panels_ipe/panels_ipe.js
@@ -3,18 +3,25 @@
 
 $(function() {
   var draggable_options = {
+    revert: 'invalid',
     helper: 'clone', // required for "flawless" interoperation with sortables
     connectToSortable: ($.ui.version === '1.6') ? ['div.panels-ipe-region'] : 'div.panels-ipe-region',
+    appendTo: 'body',
   }
 
   var sortable_options = {
     revert: true,
     opacity: 0.75, // opacity of sortable while sorting
-    placeholder: 'draggable-placeholder',
-    forcePlaceholderSize: true,
-    connectWith: 'panels-ipe-region',
+    // placeholder: 'draggable-placeholder',
+    // forcePlaceholderSize: true,
+    items: 'div.panels-ipe-pane'
   }
 
   $('div.panels-ipe-region').sortable(sortable_options);
-  $('div.panels-ipe-pane').parent().draggable(draggable_options);
+  // $('div.panels-ipe-pane').draggable(draggable_options);
+  // Since the connectWith option only does a one-way hookup, iterate over
+  // all sortable regions to connect them with one another.
+  $('div.panels-ipe-region').each(function() {
+    $(this).sortable('option', 'connectWith', ['div.panels-ipe-region'])
+  })
 })
