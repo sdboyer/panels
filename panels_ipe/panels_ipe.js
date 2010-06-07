5181335b8baf5160006409669e91cd7badbec2ab
Sam Boyer <drupal@samboyer.org>
2010-06-07 20:07:05 +0000

    Edit form AJAX/modal is working.

--- panels_ipe/panels_ipe.js
+++ panels_ipe/panels_ipe.js
@@ -1,13 +1,15 @@
 // $Id$
 
-(function ($) {
+(function($) {
   Drupal.PanelsIPE = {
     bindClickDelete: function(context) {
       $('a.pane-delete:not(.pane-delete-processed)', context)
         .addClass('pane-delete-processed')
         .click(function() {
           if (confirm('Remove this pane?')) {
-            $(this).parents('div.panels-ipe-pane').remove();
+            $(this).parents('div.panels-ipe-pane').fadeOut(1000, function() {
+              $(this).empty().remove();
+            });
           }
           return false;
         });
@@ -21,11 +23,15 @@
         appendTo: 'body',
         handle: 'panels-ipe-draghandle',
       };
-      
+
       // Add a class so that the pane content proper can be more easily identified
       $('.panels-ipe-pane > div:not(.panels-ipe-handlebar-wrapper)')
-        .addClass('panels-ipe-proper-pane');
-  
+        .addClass('panels-ipe-proper-pane'); // @todo need this to be re-added on AJAX...
+
+      // Add a class so that the direct parent container of the panes can be
+      // more easily identified
+      // $('div.panels-ipe-pane').parent().addClass('panels-ipe-region-innermost'); WHWHHAHSH
+
       /**
        * See http://jqueryui.com/demos/sortable/ for details on the configuration
        * parameters used here.
@@ -39,7 +45,7 @@
         items: 'div.panels-ipe-pane',
         handle: 'div.panels-ipe-draghandle',
       };
-  
+
       $('div.panels-ipe-region').sortable(sortable_options);
       // Since the connectWith option only does a one-way hookup, iterate over
       // all sortable regions to connect them with one another.
