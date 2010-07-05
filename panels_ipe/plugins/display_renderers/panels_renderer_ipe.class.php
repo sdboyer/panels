afd938c82be619cd7379c61c729381666f5ca169
Sam Boyer <drupal@samboyer.org>
2010-07-05 21:55:35 +0000

    Make the IPE work properly with region names that contain unfriendly characters.

--- panels_ipe/plugins/display_renderers/panels_renderer_ipe.class.php
+++ panels_ipe/plugins/display_renderers/panels_renderer_ipe.class.php
@@ -85,8 +85,8 @@ class panels_renderer_ipe extends panels_renderer_editor {
     $output = parent::render_region($region_id, $panes);
     $output = theme('panels_ipe_region_wrapper', $output, $region_id, $this->display);
     $classes = 'panels-ipe-region';
-
-    return "<div id='panels-ipe-regionid-$region_id' class='panels-ipe-region'>" . $output . "</div>";
+    $region_id = ctools_cleanstring($region_id);
+    return "<div id='panels-ipe-regionid-$region_id' class='panels-ipe-region'>$output</div>";
   }
 
   /**
@@ -156,8 +156,8 @@ class panels_renderer_ipe extends panels_renderer_editor {
     else {
       $pane = $this->display->content[$pid];
     }
-
-    $this->commands[] = ctools_ajax_command_append("#panels-ipe-regionid-$pane->panel div.panels-ipe-sort-container", $this->render_pane($pane));
+    $region_id = ctools_cleanstring($pane->panel);
+    $this->commands[] = ctools_ajax_command_append("#panels-ipe-regionid-$region_id div.panels-ipe-sort-container", $this->render_pane($pane));
 //    $this->commands[] = ctools_ajax_command_changed("#panel-pane-$pane->pid", "div.grabber span.text");
   }
 }
