86088a68f0f036f97eae26ca4897ccd42521cbd8
merlinofchaos <merlinofchaos>
2010-08-20 16:20:44 +0000

    Update javascript to account for CTools no longer using dimensions.js

--- js/display_editor.js
+++ js/display_editor.js
@@ -411,9 +411,9 @@
       }
       draggable.timeCount = draggable.timeCount + 1;
       var left = $(window).scrollLeft();
-      var right = left + $(window).innerWidth();
+      var right = left + $(window).width();
       var top = $(window).scrollTop();
-      var bottom = top + $(window).innerHeight();
+      var bottom = top + $(window).height();
 
       if (draggable.mousePos.x < left + scrollBuffer && left > 0) {
         window.scrollTo(left - scrollDistance, top);
